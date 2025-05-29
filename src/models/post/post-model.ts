38b10e765f16c602672a26f2c4147915e9b24fdb
pedro henrique <pedrohenriquebsouza.1@gmail.com>
2025-05-29 11:19:56 -0300

    models

---
+++ src/models/post/post-model.ts
@@ -0,0 +1,12 @@
+export type PostModel = {
+    id: string;
+    title: string;
+    slug: string;
+    execerpt: string;
+    content: string;
+    converImageUrl: string;
+    published: boolean;
+    createdAt: string;
+    upadateAt: string;
+    author: string;
+}
