b69a0c6a0bee26543beb166dda605ef53b42b98c
pedro henrique <pedrohenriquebsouza.1@gmail.com>
2025-05-29 10:34:34 -0300

    primeiro commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "The blog - Este é um blog com Next.js",
+  description: "Essa seria a descrição dessa página",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
